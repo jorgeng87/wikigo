e92fa6f33a24d9c91d160615b3241f60b92df78e
Jorge Núñez <jorgeng87@gmail.com>
2013-10-27 14:50:13 +0100

    Refactor to use function literal and closures

--- wiki.go
+++ wiki.go
@@ -35,12 +35,8 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
   }
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
-  p,err := loadPage(title)
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+  p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
     return
@@ -48,11 +44,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w,"view",p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
   p, err := loadPage(title)
   if err != nil {
     p = &Page{Title: title}
@@ -60,14 +52,10 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
   renderTemplate(w,"edit",p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title, err := getTitle(w, r)
-  if err != nil {
-    return
-  }
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
   body := r.FormValue("body")
   p := &Page{Title:title, Body: []byte(body)}
-  err = p.save()
+  err := p.save()
   if err != nil {
     http.Error(w, err.Error(),http.StatusInternalServerError)
   }
@@ -83,12 +71,23 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
   return m[2], nil
 }
 
+func makeHandler(fn func (http.ResponseWriter,*http.Request, string)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return
+    }
+    fn(w, r, m[2])
+  }
+}
+
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func main() {
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  http.HandleFunc("/save/", saveHandler)
+  http.HandleFunc("/view/", makeHandler(viewHandler))
+  http.HandleFunc("/edit/", makeHandler(editHandler))
+  http.HandleFunc("/save/", makeHandler(saveHandler))
   http.ListenAndServe(":8080", nil)
 }
